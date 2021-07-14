1117cead410c309c08f8ace24f801477a298bf65
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-07-14 07:48:28 +0000

    Fix remove corrupt images script

--- remove_corrupt_images.py
+++ remove_corrupt_images.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 from tqdm import tqdm
 import psutil
 import ray
@@ -8,16 +8,24 @@ path = '/home/user/data/CC12M/images'
 broken_path = '/home/user/data/CC12M/broken_images'
 num_cpus = psutil.cpu_count(logical=False)
 ray.init(num_cpus=num_cpus)
+
 @ray.remote
 def check_image(image_file):
     image_path = os.path.join(path, image_file)
     try:
-        img = read_image(image_path)
+        img = read_image(image_path, mode=ImageReadMode.RGB)
     except:
-        shutil.copy(image_path, os.path.join(broken_path, image_file))
-        shutil.remove(image_path)
+        shutil.move(image_path, broken_path)
+        os.remove(image_path)
 
-
-for idx, image_file in enumerate(os.listdir(path)):
+initial_count = len(os.listdir(path))
+print(initial_count)
+#10237845
+#10237799
+for idx, image_file in enumerate(os.listdir(path)[::-1]):
     check_image.remote(image_file)
+    #check_image(image_file)
     print(idx)
+final_count = len(os.listdir(path))
+
+print(initial_count, final_count)
