be9597fb456cca92e7b6321085a08a8c13fc6d19
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-07-15 20:03:45 +0000

    Fix shell script for sequence classification

--- run_sequence.sh
+++ run_sequence.sh
@@ -14,13 +14,13 @@ fi
 token=$(cat /home/chhablani_gunjan/.huggingface/token)
 
 echo "Token found $token"
-./run_image_text_mlm.py\
+./run_image_text_classification.py\
  --output_dir $repo\
  --data_dir ~/VQAV2\
  --train_file ./train_file_trans.tsv\
  --validation_file ./val_file_trans.tsv\
  --save_steps 5000\
- --num_train_epochs 5\
+ --num_train_epochs 10\
  --eval_steps 2500\
  --logging_steps 2500\
  --save_total_limit 5\
@@ -31,11 +31,12 @@ echo "Token found $token"
  --per_device_eval_batch_size 64\
  --warmup_steps 5000\
  --learning_rate 1e-4
+#  --max_train_samples 5000\
+#  --max_eval_samples 500\
 
 
 #  --weight_decay 0.01\
-#  --max_train_samples 5000\
-#  --max_eval_samples 500\
+
 # --resume_from_checkpoint test-vqa/ckpt-11
 # save total limit
 # learning_rate
